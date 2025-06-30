d01bd455869ea76f55d35bbb2cbb9832b9474637
Lana Frenzel <166107394+lanafrenzel@users.noreply.github.com>
2025-06-30 20:37:34 +0200

    Initial commit of Terraform project

---
+++ providers.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project = "komma5-464511"
+  region  = "us-central1"
+}
---
+++ terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    google = {
+      version = "~> 6.0"
+      source  = "hashicorp/google"
+    }
+  }
+}
